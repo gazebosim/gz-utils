6a5604f62cf302bf948babaa86b57707c1e77e7a
Michael Carroll <michael@openrobotics.org>
2020-12-22 08:14:49 -0600

    Documentation
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- include/ignition/utils/ImplPtr.hh
+++ include/ignition/utils/ImplPtr.hh
@@ -216,23 +216,31 @@ namespace ignition
   }  // namespace utils
 }  // namespace ignition
 
+/// \brief Add a private ImplPtr to a class as dataPtr.
+///  This variant forward-declares Class::Implementation as the impl.
 #define IGN_UTILS_IMPL_PTR \
   public: class Implementation; \
   IGN_UTILS_WARN_IGNORE__DLL_INTERFACE_MISSING \
   private: ::ignition::utils::ImplPtr<Implementation> dataPtr; \
   IGN_UTILS_WARN_RESUME__DLL_INTERFACE_MISSING
 
+/// \brief Add a private UniqueImplPtr to a class as dataPtr.
+///  This variant forward-declares Class::Implementation as the impl.
 #define IGN_UTILS_UNIQUE_IMPL_PTR \
   public: class Implementation; \
   IGN_UTILS_WARN_IGNORE__DLL_INTERFACE_MISSING \
   private: ::ignition::utils::UniqueImplPtr<Implementation> dataPtr; \
   IGN_UTILS_WARN_RESUME__DLL_INTERFACE_MISSING
 
+/// \brief Add a private ImplPtr to a class as dataPtr.
+///  This variant takes a forward-declared ImplementationClass as the impl.
 #define IGN_UTILS_IMPL_PTR_FWD(ImplementationClass) \
   IGN_UTILS_WARN_IGNORE__DLL_INTERFACE_MISSING \
   private: ::ignition::utils::ImplPtr<ImplementationClass> dataPtr; \
   IGN_UTILS_WARN_RESUME__DLL_INTERFACE_MISSING
 
+/// \brief Add a private UniqueImplPtr to a class as dataPtr.
+///  This variant takes a forward-declared ImplementationClass as the impl.
 #define IGN_UTILS_UNIQUE_IMPL_PTR_FWD(ImplementationClass) \
   IGN_UTILS_WARN_IGNORE__DLL_INTERFACE_MISSING \
   private: ::ignition::utils::UniqueImplPtr<ImplementationClass> dataPtr; \
