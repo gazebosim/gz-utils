037638e5476a39201feff40ca8dd47d17032c742
Michael Carroll <mjcarroll@intrinsic.ai>
2023-06-27 18:50:57 +0000

    Add subprocess utilities
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

---
+++ test/integration/subprocess/subprocess_main.cc
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2023 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <gz/utils/cli/CLI.hpp>
+
+class OutputSink
+{
+  public: OutputSink(const std::string &_dest):
+    dest(_dest)
+  {
+  }
+
+  public: void Write(const std::string& val)
+  {
+    if (dest == "cout" || dest == "both")
+    {
+      std::cout << val << std::endl;
+
+    }
+    else if (dest == "cerr" || dest == "both")
+    {
+      std::cerr << val << std::endl;
+    }
+    return;
+  }
+
+  private: std::string dest;
+};
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  CLI::App app("Subprocess test app");
+
+  std::string output;
+  app.add_option("--output", output, "output destination");
+
+  int iters;
+  app.add_option("--iterations", iters, "number of iterations to run");
+
+  int iter_ms;
+  app.add_option("--iteration-ms", iter_ms, "length of one iteration");
+
+  CLI11_PARSE(app, argc, argv);
+
+  auto sink = OutputSink(output);
+  for (int ii = 0; ii < iters; ++ii)
+  {
+    sink.Write("Iteration: " + std::to_string(ii));
+    std::this_thread::sleep_for(std::chrono::milliseconds(iter_ms));
+  }
+
+}
---
+++ test/integration/subprocess_TEST.cc
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+
+#include <gz/utils/Subprocess.hh>
+
+#include <chrono>
+#include <thread>
+
+using Subprocess = gz::utils::Subprocess;
+
+static constexpr const char * kExecutablePath = SUBPROCESS_EXECUTABLE_PATH;
+
+/////////////////////////////////////////////////
+TEST(Subprocess, CreateInvalid)
+{
+  auto proc = Subprocess({"an_executable_that_will_never_exist"});
+  EXPECT_FALSE(proc.Alive());
+}
+
+/////////////////////////////////////////////////
+TEST(Subprocess, CreateValid)
+{
+  auto proc = Subprocess({kExecutablePath, "--help"});
+
+  // Sleep for just a bit to guarantee executable finishes
+  std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  EXPECT_FALSE(proc.Alive());
+
+  auto cout = proc.Stdout();
+  auto cerr = proc.Stdout();
+
+  EXPECT_FALSE(cout.empty());
+  EXPECT_TRUE(cerr.empty());
+
+  auto ret = proc.Join();
+  EXPECT_EQ(0u, ret);
+}
+
+/////////////////////////////////////////////////
+TEST(Subprocess, Cout)
+{
+  auto proc = Subprocess({kExecutablePath,
+                          "--output=cout",
+                          "--iterations=10",
+                          "--iteration-ms=10"});
+
+  // Sleep for just a bit to guarantee executable finishes
+  EXPECT_TRUE(proc.Alive());
+
+  // Block until the executable is done
+  auto ret = proc.Join();
+  EXPECT_EQ(0u, ret);
+
+  auto cout = proc.Stdout();
+  auto cerr = proc.Stderr();
+  EXPECT_FALSE(cout.empty());
+  EXPECT_TRUE(cerr.empty());
+}
+
+/////////////////////////////////////////////////
+TEST(Subprocess, Cerr)
+{
+  auto proc = Subprocess({kExecutablePath,
+                          "--output=cerr",
+                          "--iterations=10",
+                          "--iteration-ms=10"});
+  EXPECT_TRUE(proc.Alive());
+
+  // Block until the executable is done
+  auto ret = proc.Join();
+  EXPECT_EQ(0u, ret);
+
+  EXPECT_FALSE(proc.Alive());
+  auto cout = proc.Stdout();
+  auto cerr = proc.Stderr();
+  EXPECT_TRUE(cout.empty());
+  EXPECT_FALSE(cerr.empty());
+}
