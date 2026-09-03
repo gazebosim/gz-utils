ad626122ff676c081de9f46441fc721bab76f128
Addisu Z. Taddese <addisuzt@intrinsic.ai>
2026-09-03 09:49:06 -0500

    Remove duplicated code
    Signed-off-by: Addisu Z. Taddese <addisuzt@intrinsic.ai>

--- include/gz/utils/SuppressWarning.hh
+++ include/gz/utils/SuppressWarning.hh
@@ -98,19 +98,6 @@
     DETAIL_GZ_UTILS_WARN_RESUME__SWITCH_NO_DEFAULT_STATEMENT
 #endif
 
-/// \brief Use this to suppress warnings for switch without a default statement.
-/// This can be useful when using code that relies on not having a default to
-/// trigger missing case warnings when a new enum variant is added.
-#ifndef GZ_UTILS_WARN_IGNORE__SWITCH_NO_DEFAULT_STATEMENT
-  #define GZ_UTILS_WARN_IGNORE__SWITCH_NO_DEFAULT_STATEMENT \
-    DETAIL_GZ_UTILS_WARN_IGNORE__SWITCH_NO_DEFAULT_STATEMENT
-#endif
-
-#ifndef GZ_UTILS_WARN_RESUME__SWITCH_NO_DEFAULT_STATEMENT
-  #define GZ_UTILS_WARN_RESUME__SWITCH_NO_DEFAULT_STATEMENT \
-    DETAIL_GZ_UTILS_WARN_RESUME__SWITCH_NO_DEFAULT_STATEMENT
-#endif
-
 /// \brief Use this to suppress warnings for unused functions.
 /// This can be useful when external libraries have static functions with
 /// internal linkage that are not used.
